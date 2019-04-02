f5f7cc8d5d77e932f2d8a71d9c92f1f2ddbf0341
jculver1 <jculver1900@gmail.com>
2019-04-02 13:48:06 -0600

    added fetch request to get the usda_id, need to save to state

--- eatit/components/ItemDetails.js
+++ eatit/components/ItemDetails.js
@@ -5,8 +5,25 @@ class ItemDetails extends React.Component {
 
   foodIdentified = this.props.navigation.state.params.foodItem
 
+  foodsRoute = `http://localhost:3001/foods/${this.foodIdentified}`
+
+  getFoodName() {
+    return fetch(this.foodsRoute)
+      .then((response) => response.json())
+      .then((responseJson) => {  
+        this.setState({
+          food_id: responseJson[0].usda_id
+        })
+        return responseJson.usda_id;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
-    console.log(this.foodIdentified)
+    this.getFoodName()
+    // console.log(this.state.food_id)
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
         <Text>Check out the nutrients in {this.foodIdentified}!</Text> 
