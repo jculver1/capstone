83308873a32de0ff2a023498d01c9fb4bd432398
jculver1 <jculver1900@gmail.com>
2019-04-03 20:42:52 -0600

    added example scrollview

--- eatit/components/ItemDetails.js
+++ eatit/components/ItemDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 class ItemDetails extends React.Component {
 
@@ -7,11 +8,6 @@ class ItemDetails extends React.Component {
 		super(props);
      
     }
-
-    // componentDidMount(){
-    //   this.getFoods()
-    //   this.getNutrients()
-    // }
 
   foodIdentified = this.props.navigation.state.params.foodItem
 
@@ -25,7 +21,8 @@ class ItemDetails extends React.Component {
       return fetch(`https://api.nal.usda.gov/ndb/reports/?ndbno=${food_id}&type=f&format=json&api_key=g03EsNMIdLVGVFxer9G0rkguZEPyUf2dcDyxlKH6&nutrients=205&nutrients=204&nutrients=208&nutrients=269`)
       .then(respons => respons.json())
       .then(responseJson => {
-        console.log(responseJson, 'hi')
+        this.setState({nutrientData : responseJson.report.food})
+        console.log(this.state.nutrientData)
       })
       .catch(error => {
         console.error(error)
@@ -34,12 +31,28 @@ class ItemDetails extends React.Component {
   }
 
   render() {
+
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
         <Text>Check out the nutrients in {this.foodIdentified}!</Text> 
+        <ScrollView>
+          <Text style={styles.welcome}>scrollview</Text>
+        </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  welcome: {
+    flex: 1,
+    margin: 20,
+    backgroundColor: 'orange',
+    margin: 10,
+    textAlign: 'center',
+    fontSize: 20,
+    paddingTop: 70,
+  }
+});
 
 export default ItemDetails;
